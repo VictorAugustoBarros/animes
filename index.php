0a3175b109247fde6cd872d1780741f5182a2599
victor <victor.barros@pgmais.com.br>
2019-12-27 10:01:04 -0300

    ajuste index

--- index.php
+++ index.php
@@ -7,9 +7,17 @@ header('Content-Type: text/html; charset=utf-8');
 
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
 <div class="site-wrap">
+
     <?php include($_SERVER['DOCUMENT_ROOT'] . "/mvc/views/top_bar.php"); ?>
 
-    <?php include($_SERVER['DOCUMENT_ROOT'] . "/mvc/views/animes/listar_animes.php"); ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/mvc/views/page_1.php"); ?>
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/mvc/views/page_2.php"); ?>
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/mvc/views/page_3.php"); ?>
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/mvc/views/footer.php"); ?>
+
 </div>
 
 <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a>
